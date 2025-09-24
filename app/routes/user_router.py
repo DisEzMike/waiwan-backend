da2e7ebac1d7ef873f51d34871ebb0e7742d4031
Disezmike <mikenatchapon@gmail.com>
2025-09-24 18:23:07 +0700

    add User response

--- app/routes/user_router.py
+++ app/routes/user_router.py
@@ -21,32 +21,68 @@ def get_me(ctx = Depends(get_current_user), session: Session = Depends(get_db)):
             role=user.role,
             displayname=user.displayname,
             profile_id=user.profile_id,
-            ability_id=user.ability_id if ability else None,
+            ability_id=user.ability_id if user.role == "senior_user" else None,
+            created_at=user.created_at
         ),
-        profile=(ProfileOut(
+        profile=ProfileOut(
             id=profile.id,
             first_name=profile.first_name,
             last_name=profile.last_name,
+            id_card=profile.id_card if user.role == "senior_user" else None,
+            addr_from_id=profile.addr_from_id if user.role == "senior_user" else None,
+            addr_current=profile.addr_current if user.role == "senior_user" else None,
             phone=profile.phone,
-            gender=profile.gender
-        ) if profile else None),
+            gender=profile.gender,
+            underlying_diseases=profile.underlying_diseases if user.role == "senior_user" else None,
+            contact_person=profile.contact_person if user.role == "senior_user" else None,
+            contact_phone=profile.contact_phone if user.role == "senior_user" else None,
+        ),
         ability=(AbilityOut(
             id=ability.id,
+            type=ability.type,
             career=ability.career,
-            other_ability=ability.other_ability
-        ) if ability else None),
-    ) 
+            other_ability=ability.other_ability,
+            vehicle=ability.vehicle,
+            offsite_work=ability.offsite_work 
+        ) if ability else None)
+    )
     
 @router.get("/{user_id}")
 async def get_user(user_id: int, ctx = Depends(get_current_user), session: Session = Depends(get_db)):
     user: SeniorUsers | None = getUser_by_id(user_id, session)
     if user is None : raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    profile: SeniorProfiles | None = getProfile_by_id(user.profile_id)
-    ability: SeniorAbilities | None = getAbility_by_id(user.ability_id)
-    return UserOut(
-        id=user.id,
-        displayname=user.displayname,
-        
+    profile: SeniorProfiles = user.profile
+    ability: SeniorAbilities = user.ability
+    return UserResponse(
+        user=UserOut(
+            id=user.id,
+            role="senior_user",
+            displayname=user.displayname,
+            profile_id=user.profile_id,
+            ability_id=user.ability_id,
+            created_at=user.created_at
+        ),
+        profile=ProfileOut(
+            id=profile.id,
+            first_name=profile.first_name,
+            last_name=profile.last_name,
+            id_card=profile.id_card,
+            addr_from_id=profile.addr_from_id,
+            addr_current=profile.addr_current,
+            phone=profile.phone,
+            gender=profile.gender,
+            underlying_diseases=profile.underlying_diseases,
+            contact_person=profile.contact_person,
+            contact_phone=profile.contact_phone,
+        ),
+        ability=AbilityOut(
+            id=ability.id,
+            type=ability.type,
+            career=ability.career,
+            other_ability=ability.other_ability,
+            vehicle=ability.vehicle,
+            offsite_work=ability.offsite_work 
+        )
     )
 
 @router.post("/set-online", status_code=status.HTTP_204_NO_CONTENT)
